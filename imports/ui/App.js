84f00391a5a02b9fa65e6ec159a4d5f79dfccd9e
Emilio Lombana <emiliolombana@gmail.com>
2018-11-02 16:41:08 -0700

    rm

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,21 +2,6 @@ import React, { Component } from "react";
 import Game from "../ui/components/game";
 import Canvas from "../ui/components/canvas";
 
-<<<<<<< HEAD
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <Canvas />
-      </div>
-    );
-  }
-}
-=======
 import { Players } from '../api/players';
 import { Gameboard } from '../api/gameboard';
 
@@ -25,6 +10,5 @@ const App = () => (
         <Layout />
     </Router>
 );
->>>>>>> 01abb2653e995995aa09c11cdd1ff5ba36629b4f
 
 export default App;
