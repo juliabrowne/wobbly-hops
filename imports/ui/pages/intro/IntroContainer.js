7534e87dc221c9107633837d42c72178a01bee02
Emilio Lombana <emiliolombana@gmail.com>
2018-11-01 22:58:07 -0700

    Fix spacing issue on start page

--- imports/ui/pages/intro/IntroContainer.js
+++ imports/ui/pages/intro/IntroContainer.js
@@ -3,31 +3,31 @@ import './styles.css'
 
 class IntroContainer extends Component {
     render() {
-        return <div className="root">
-            <h1 className="header">W🤪BBLY-H🤮PS🍻</h1>
-            <button className="start-button">START</button>
-            <button className="start-button">INSTRUCTIONS</button>
-            <div id="bubbles">
-              <div className="bubble x1" />
-              <div className="bubble x2" />
-              <div className="bubble x3" />
-              <div className="bubble x4" />
-              <div className="bubble x5" />
-            </div>
-            <div id="bubbles">
-              <div className="bubble x1" />
-              <div className="bubble x2" />
-              <div className="bubble x3" />
-              <div className="bubble x4" />
-              <div className="bubble x5" />
-              <div className="bubble x6" />
-              <div className="bubble x7" />
-              <div className="bubble x8" />
-              <div className="bubble x9" />
-              <div className="bubble x10" />
-              <div className="bubble x11" />
-            </div>
-          </div>;
+      return <div className="root">
+          <h1 className="header">W🤪BBLY-H🤮PS🍻</h1>
+          <button className="start-button">Start</button>
+          <button className="start-button">The Rules</button>
+          <div id="bubbles">
+            <div className="bubble x1" />
+            <div className="bubble x2" />
+            <div className="bubble x3" />
+            <div className="bubble x4" />
+            <div className="bubble x5" />
+          </div>
+          <div id="bubbles">
+            <div className="bubble x1" />
+            <div className="bubble x2" />
+            <div className="bubble x3" />
+            <div className="bubble x4" />
+            <div className="bubble x5" />
+            <div className="bubble x6" />
+            <div className="bubble x7" />
+            <div className="bubble x8" />
+            <div className="bubble x9" />
+            <div className="bubble x10" />
+            <div className="bubble x11" />
+          </div>
+        </div>;
     }
 }
 
