fa9317735d4a45ab7ea29c3411cf7363c735a1d7
Emilio Lombana <emiliolombana@gmail.com>
2018-11-01 22:20:04 -0700

    Add particle effect to start screen

--- imports/ui/pages/intro/IntroContainer.js
+++ imports/ui/pages/intro/IntroContainer.js
@@ -7,6 +7,26 @@ class IntroContainer extends Component {
             <h1 className="header">W🤪BBLY-H🤮PS🍻</h1>
             <button className="start-button">START</button>
             <button className="start-button">INSTRUCTIONS</button>
+            <div id="bubbles">
+              <div className="bubble x1" />
+              <div className="bubble x2" />
+              <div className="bubble x3" />
+              <div className="bubble x4" />
+              <div className="bubble x5" />
+            </div>
+            <div id="bubbles">
+              <div className="bubble x1" />
+              <div className="bubble x2" />
+              <div className="bubble x3" />
+              <div className="bubble x4" />
+              <div className="bubble x5" />
+              <div className="bubble x6" />
+              <div className="bubble x7" />
+              <div className="bubble x8" />
+              <div className="bubble x9" />
+              <div className="bubble x10" />
+              <div className="bubble x11" />
+            </div>
           </div>;
     }
 }
