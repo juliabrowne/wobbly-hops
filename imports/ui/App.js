5acd7f457153f51a42e23fe8f7ded337b989574c
Owen Delisle <owendelisle@Owens-MacBook-Pro.local>
2018-11-02 16:16:17 -0700

    Resolving Merge Conflicts

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Game from "../ui/components/game";
 import Canvas from "../ui/components/canvas";
 
-<<<<<<< HEAD
 class App extends Component {
   constructor() {
     super();
@@ -16,15 +15,5 @@ class App extends Component {
     );
   }
 }
-=======
-import { Players } from '../api/players';
-import { Gameboard } from '../api/gameboard';
-
-const App = () => (
-    <Router>
-        <Layout />
-    </Router>
-);
->>>>>>> 01abb2653e995995aa09c11cdd1ff5ba36629b4f
 
 export default App;
