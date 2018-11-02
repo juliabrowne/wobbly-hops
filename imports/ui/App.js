d9eed576959c8505c14f9adb249ee07a3c7d308c
Juett <juettdm@gmail.com>
2018-11-02 16:38:11 -0700

    RM

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,29 +1,11 @@
-import React, { Component } from "react";
-import Game from "../ui/components/game";
-import Canvas from "../ui/components/canvas";
-
-<<<<<<< HEAD
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <Canvas />
-      </div>
-    );
-  }
-}
-=======
-import { Players } from '../api/players';
+import React from "react";
+import {BrowserRouter as Router} from 'react-router-dom'
+import Layout from '../../routes/layout'
 
 const App = () => (
     <Router>
         <Layout />
     </Router>
 );
->>>>>>> 01abb2653e995995aa09c11cdd1ff5ba36629b4f
 
 export default App;
