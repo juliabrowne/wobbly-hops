505d81ae4bc66cda75c2c711b6cb80890e944569
Owen Delisle <owen37164@gmail.com>
2018-11-05 10:17:50 -0800

    Commiting to fix everyones local copys

--- imports/ui/components/canvas/index.js
+++ imports/ui/components/canvas/index.js
@@ -9,14 +9,36 @@ class Canvas extends React.Component {
     this.canvasRef = React.createRef();
     this.paddles = [];
     this.players = [];
+    this.direction = {};
+    this.collision = false;
   }
 
   componentDidMount() {
+    window.onkeydown = e => {
+      this.direction[e.key] = true;
+    };
+    window.onkeyup = e => {
+      delete this.direction[e.key];
+    };
     this.ctx = this.canvasRef.current.getContext("2d");
     // this.generatePaddles();
     setInterval(() => requestAnimationFrame(() => this.gameLoop()), 16);
     console.log(this.canvasRef.current.width, this.canvasRef.current.height);
   }
+  move = () => {
+    if ("ArrowRight" in this.direction) {
+      // if (this.positionX >= this.canvas.width) {
+      //   this.positionX = 0;
+      // }
+      this.players[0].positionX += this.players[0].velocityX;
+    }
+    if ("ArrowLeft" in this.direction) {
+      // if (this.positionX <= 0) {
+      //   this.positionX = this.canvas.width;
+      // }
+      this.players[0].positionX -= this.players[0].velocityX;
+    }
+  };
 
   gameLoop() {
     this.ctx.fillStyle = "white";
@@ -28,8 +50,25 @@ class Canvas extends React.Component {
     );
     this.renderPaddles();
     this.renderPlayers();
-    // this.checkCollision(this.paddles, this.players);
+    this.move();
+    this.bounce();
+    this.checkCollision(this.paddles);
   }
+  bounce = () => {
+    if (
+      this.players[0].positionY >=
+        this.canvasRef.current.height - this.players[0].radius ||
+      this.collision === true
+    ) {
+      this.players[0].velocityY = this.players[0].velocityY * -1;
+
+      setTimeout(() => {
+        this.players[0].velocityY = this.players[0].velocityY * -1;
+      }, 750);
+    }
+    this.players[0].positionY += this.players[0].velocityY;
+    this.collision = false;
+  };
 
   renderPaddles() {
     this.paddles = this.paddles.filter(p => p.destroy === false);
@@ -40,15 +79,17 @@ class Canvas extends React.Component {
   }
 
   renderPlayers() {
-    this.players.forEach(p => {
-      p.render(this.ctx);
-    });
+    // this.players.forEach(p => {
+    //   p.render(this.ctx);
+    // });
+
     this.generatePlayers();
+    this.players[0].render(this.ctx);
   }
 
   generatePaddles() {
-    if (this.paddles.length < 5) {
-      for (let i = 0; i < 5 - this.paddles.length; i++) {
+    if (this.paddles.length < 15) {
+      for (let i = 0; i < 15 - this.paddles.length; i++) {
         this.paddles.push(
           new Paddle({
             position: {
@@ -68,26 +109,33 @@ class Canvas extends React.Component {
         position: {
           x: this.canvasRef.current.width / 2,
           y: this.canvasRef.current.height / 2
-        }
+        },
+        moveDirection: ""
       })
     );
   }
 
-  // checkCollision(paddles, players) {
-  //   // The objects are touching
-  //   this.players.forEach(player => {
-  //     this.paddles.forEach(paddle => {
-  //       if (
-  //         player.position.x < paddle.position.x + paddle.width &&
-  //         player.position.x + player.width > paddle.position.x &&
-  //         player.position.y < paddle.position.y + paddle.height &&
-  //         player.position.y + player.height > paddle.position.y
-  //       ) {
-  //         console.log("Touching");
-  //       }
-  //     });
-  //   });
-  // }
+  checkCollision(paddles) {
+    // The objects are touching
+
+    //   if (object1.x < object2.x + object2.width  && object1.x + object1.width  > object2.x &&
+    //     object1.y < object2.y + object2.height && object1.y + object1.height > object2.y) {
+    // // The objects are touching
+    // }
+
+    this.paddles.forEach(paddle => {
+      if (
+        this.players[0].positionX < paddle.position.x + paddle.width &&
+        this.players[0].positionX + this.players[0].radius >
+          paddle.position.x &&
+        this.players[0].positionY < paddle.position.y + paddle.height &&
+        this.players[0].positionY + this.players[0].radius > paddle.position.y
+      ) {
+        console.log("Bounce");
+        this.collision = true;
+      }
+    });
+  }
 
   render() {
     return (
--- imports/ui/components/player/index.js
+++ imports/ui/components/player/index.js
@@ -3,19 +3,36 @@ import React from "react";
 export default class Player extends React.Component {
   constructor(args) {
     super(args);
-    this.position = args.position;
-    this.wh = args.wh;
-    this.width = 100;
-    this.height = 100;
-    this.velocity = 1;
-    this.destroy = false;
+    this.velocityY = 3;
+    this.velocityX = 3;
+    this.positionX = 100;
+    this.positionY = 0;
+    this.radius = 50;
+    this.ball;
   }
+  drawPlayer = () => {
+    this.ctx.fillStyle = "white";
+    this.ctx.beginPath();
+    this.ctx.arc(this.positionX, this.positionY, 50, 0, Math.PI * 2, true);
+    this.ctx.fill();
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = "black";
+    this.ctx.stroke();
+  };
   render(ctx) {
-    // this.position.y = this.position.y + this.velocity;
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    if (this.position.y > this.wh) {
-      this.destroy = true;
+    this.positionY = this.positionY + this.velocityY;
+    if (this.moveDirection === "right") {
+      this.positionX = this.positionX + this.velocityX;
     }
+    if (this.moveDirection === "left") {
+      this.positionX = this.positionX - this.velocityX;
+    }
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.positionX, this.positionY, 50, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
   }
 }
