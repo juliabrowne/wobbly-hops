15be1e98582aa030e64569dcd8a624b26a46721e
Juett <juettdm@gmail.com>
2018-11-05 10:19:03 -0800

    RM

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,23 +2,6 @@ import React from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
 import Layout from '../../routes/layout'
 
-
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <Canvas />
-      </div>
-    );
-  }
-}
-
-import { Players } from '../api/players';
-
 const App = () => (
     <Router>
         <Layout />
