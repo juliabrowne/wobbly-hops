7486b699fc213d194daa4050022d31d2df5203d9
Julia Browne <julia.browne@live.ca>
2018-11-05 16:35:07 -0800

    add user to database, work on players.js and container files

--- imports/ui/pages/createChar/createCharContainer.js
+++ imports/ui/pages/createChar/createCharContainer.js
@@ -4,18 +4,35 @@ import ReactAudioPlayer from "react-audio-player";
 import { withTracker } from "meteor/react-meteor-data";
 import { Players } from "../../../api/players";
 
+const SubmitButton = ({ addPlayer }) => {
+  return <button onClick={() => addPlayer()}>Lets Play!</button>;
+};
+
 class createCharContainer extends Component {
-  addPlayer = (player) => {
-    Meteor.call("add.player", player);
+  constructor() {
+    super(); 
+    this.nameInput = React.createRef();
   }
+
+
+  addPlayer = () => {
+    event.preventDefault();
+    let nameInput = this.nameInput.current;
+    if (nameInput.value) {
+      Meteor.call('add.player', this.nameInput.current.value)
+    }
+  }
+
+
+
     render() {
         return <div className="root">
             <ReactAudioPlayer src="../../../music/bensound-allthat.mp3" autoPlay loop />
             <h1 className="header">W🤪BBLY-H🤮PS🍻</h1>
-            <form className="inputDiv">
+            <form onSubmit = {this.addPlayer} className="inputDiv">
               <h2 className="name-header">Whats ya' name?</h2>
-              <input type="text" className="input" maxLength="12" />
-              <button className="submit" onClick={() => add.player()}>Lets Play!</button>
+              <input type="text" className="input" maxLength="12" ref={this.nameInput}/>
+              <SubmitButton className="submit" addPlayer={this.addPlayer} />
             </form>
             <div id="bubbles">
               <div className="bubble x1" />
@@ -33,14 +50,13 @@ class createCharContainer extends Component {
           </div>;
    }
 }
-<<<<<<< HEAD
 
-export default createCharContainer;
-=======
+
 export default withTracker(() => {
   Meteor.subscribe("player");
   return {
+    currentUser: Meteor.user(),
+    currentUserId: Meteor.userId(),
     player: Players.find().fetch()
   };
 })(createCharContainer);
->>>>>>> d47dbf1d4720ea1eb83a25d28722dbc7d31b4f3f
