15c35e892b40e044ea493b4bab598bc7d1e049de
Julia Browne <julia.browne@live.ca>
2018-11-07 10:48:41 -0800

    fix controller index.js file, add arrow images

--- imports/ui/components/controller/index.js
+++ imports/ui/components/controller/index.js
@@ -8,24 +8,50 @@ import {
 import {
     Players
 } from "../../../api/players";
+import { Link } from "react-router-dom";
+import { Meteor } from "meteor/meteor";
 
+const MoveRightButton = ({ moveRight }) => {
+    return <button className='rightButton' onClick={() => moveRight()}><img src='./../../../controllerImages/rightArrow.png' /></button>;
+};
+const MoveLeftButton = ({ moveLeft }) => {
+    return <button className='leftButton' onClick={() => moveLeft()}><img src='./../../../controllerImages/leftArrow.png' /></button>;
+};
 
 class Controller extends Component {
     constructor() {
-
+        super();
     }
+
+    moveRight = () => {
+        event.preventDefault();
+        Meteor.call(
+            'move.right',
+            Meteor.userId()
+        );
+    };
+
+    moveLeft = () => {
+        event.preventDefault();
+        Meteor.call(
+            'move.left',
+            Meteor.userId()
+        );
+    };
 
     render() {
         return <div className='root'>
+            <div className='buttonRow'>
+                <MoveLeftButton moveLeft={this.moveLeft} />
+                <MoveRightButton moveRight={this.moveRight} />
+            </div>
+
             <Link to='/intro'>
               <p className="return">Return to front page!</p>
             </Link>
-
-            <button onClick={this.moveRight} className='rightButton'></button>
-
-
-            <button onClick={this.moveLeft} className='leftButton'> </button>
-      }
-
-
+        </div>
+    }
 }
+
+
+export default Controller;
