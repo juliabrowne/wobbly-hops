c26d267f619ba7f2dff2b4129c24014efdd16dcd
Juett <juettdm@gmail.com>
2018-11-07 15:12:15 -0800

    add meteor calls to controller and update createCharacter

--- imports/ui/components/controller/index.js
+++ imports/ui/components/controller/index.js
@@ -1,57 +1,64 @@
-import React, {
-    Component
-} from "react";
-import './styles';
-import {
-    withTracker
-} from "meteor/react-meteor-data";
-import {
-    Players
-} from "../../../api/players";
+import React, { Component } from "react";
+import "./styles";
+import { withTracker } from "meteor/react-meteor-data";
+import { Players } from "../../../api/players";
 import { Link } from "react-router-dom";
 import { Meteor } from "meteor/meteor";
 
 const MoveRightButton = ({ moveRight }) => {
-    return <button className='rightButton' onClick={() => moveRight()}><img src='./../../../controllerImages/rightArrow.png' /></button>;
+  return (
+    <button className="rightButton" onClick={() => moveRight()}>
+      <img src="./../../../controllerImages/rightArrow.png" />
+    </button>
+  );
 };
 const MoveLeftButton = ({ moveLeft }) => {
-    return <button className='leftButton' onClick={() => moveLeft()}><img src='./../../../controllerImages/leftArrow.png' /></button>;
+  return (
+    <button className="leftButton" onClick={() => moveLeft()}>
+      <img src="./../../../controllerImages/leftArrow.png" />
+    </button>
+  );
 };
 
 class Controller extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    moveRight = () => {
-        event.preventDefault();
-        Meteor.call(
-            'move.right',
-            Meteor.userId()
-        );
-    };
+  moveRight = player => {
+    console.log(player);
+    Meteor.call("move.right", player._id);
+  };
 
-    moveLeft = () => {
-        event.preventDefault();
-        Meteor.call(
-            'move.left',
-            Meteor.userId()
-        );
-    };
+  moveLeft = player => {
+    console.log(player);
+    Meteor.call("move.left", player._id);
+  };
 
-    render() {
-        return <div className='root'>
-            <div className='buttonRow'>
-                <MoveLeftButton moveLeft={this.moveLeft} />
-                <MoveRightButton moveRight={this.moveRight} />
-            </div>
-
-            <Link to='/intro'>
-              <p className="return">Return to front page!</p>
-            </Link>
+  render() {
+    console.log(this.props);
+    const { currentPlayer } = this.props;
+    return (
+      <div className="root">
+        <div className="buttonRow">
+          <MoveLeftButton moveLeft={() => this.moveLeft(currentPlayer[0])} />
+          <MoveRightButton moveRight={() => this.moveRight(currentPlayer[0])} />
         </div>
-    }
+
+        <Link to="/intro">
+          <p className="return">Return to front page!</p>
+        </Link>
+      </div>
+    );
+  }
 }
 
-
-export default Controller;
+export default withTracker(() => {
+  const handle = Meteor.subscribe("players");
+  const userId = Meteor.userId();
+  const currentPlayer = Players.find({playerId: userId}).fetch();
+   return {
+    loading: !handle.ready(),
+    currentPlayer
+  };
+})(Controller);
--- imports/ui/pages/createChar/createCharContainer.js
+++ imports/ui/pages/createChar/createCharContainer.js
@@ -4,10 +4,14 @@ import ReactAudioPlayer from "react-audio-player";
 import { withTracker } from "meteor/react-meteor-data";
 import { Players } from "../../../api/players";
 import { Link } from "react-router-dom";
-import { CirclePicker } from 'react-color';
+import { CirclePicker } from "react-color";
 
 const SubmitButton = ({ addPlayer }) => {
-  return <button className="submit" onClick={() => addPlayer()}>Lets Play!</button>;
+  return (
+    <button className="submit" onClick={() => addPlayer()}>
+      Lets Play!
+    </button>
+  );
 };
 
 class createCharContainer extends Component {
@@ -17,39 +21,47 @@ class createCharContainer extends Component {
     this.colorInput = React.createRef();
   }
 
-
-
   addPlayer = () => {
     event.preventDefault();
     let nameInput = this.nameInput.current;
     let colorInput = this.colorInput.current;
-    if (nameInput.value && colorInput.value) {
+    console.log(colorInput)
+    if (nameInput.value && colorInput.state.hex) {
       Meteor.call(
         "add.player",
         this.nameInput.current.value,
-        this.colorInput.current.value,
-        Meteor.userId()
+        colorInput.state.hex,
+        Meteor.userId(),
       );
     }
   };
 
-
   render() {
-    return <div className="root">
-        <ReactAudioPlayer src="../../../music/bensound-allthat.mp3" autoPlay loop />
+    return (
+      <div className="root">
+        <ReactAudioPlayer
+          src="../../../music/bensound-allthat.mp3"
+          autoPlay
+          loop
+        />
         <Link to="/intro">
           <h1 className="header">W🤪BBLY-H🤮PS🍻</h1>
         </Link>
         <form onSubmit={this.addPlayer} className="inputDiv">
           <h2 className="name-header">What's ya' name?</h2>
-          <input type="text" className="input" maxLength="12" ref={this.nameInput} />
+          <input
+            type="text"
+            className="input"
+            maxLength="12"
+            ref={this.nameInput}
+          />
           <h2 className="name-header">What's ya' favorite color?</h2>
           <h4 className="center">(Click to Change)</h4>
           <div className="center">
             <CirclePicker className="color-select" ref={this.colorInput} />
           </div>
           <div className="center">
-            <SubmitButton addPlayer={this.addPlayer} />
+            <SubmitButton addPlayer={() => this.addPlayer()} />
           </div>
         </form>
         <div id="bubbles">
@@ -64,10 +76,10 @@ class createCharContainer extends Component {
           <div className="bubble x9" />
           <div className="bubble x10" />
         </div>
-      </div>;
+      </div>
+    );
   }
 }
-
 
 export default withTracker(() => {
   const handle = Meteor.subscribe("players");
