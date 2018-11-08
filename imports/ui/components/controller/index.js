fb0eddeca062bfa7551d630aa088a07fe04ae4ef
Julia Browne <julia.browne@live.ca>
2018-11-08 12:08:27 -0800

    change controller index.js to try and make onclick function continuous

--- imports/ui/components/controller/index.js
+++ imports/ui/components/controller/index.js
@@ -2,36 +2,35 @@ import React, { Component } from "react";
 import "./styles";
 import { withTracker } from "meteor/react-meteor-data";
 import { Players } from "../../../api/players";
-import { Link } from "react-router-dom";
 import { Meteor } from "meteor/meteor";
-import Player from "../player";
-import Paddle from "../paddle";
+import MetaTags from 'react-meta-tags';
 
 const MoveRightButton = ({ moveRight }) => {
   return (
-    <button className='rightButton' onMouseDown={() => moveRight()}>
+    <button className='rightButton' onClick={() => moveRight()}>
       <img src='./../../../controllerImages/rightArrow.png' />
     </button>
   );
 };
 const MoveLeftButton = ({ moveLeft }) => {
   return (
-    <button className='leftButton' onMouseDown={() => moveLeft()}>
+    <button className='leftButton' onClick={() => moveLeft()}>
       <img src='./../../../controllerImages/leftArrow.png' />
     </button>
   );
 };
 
 const rowStyle = {
-  display: "flex",
-  justifyContent: "space-around",
-  flexDirection: "row",
-  minHeight: "100vh"
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexDirection: 'row',
+  height: '100vh', 
 };
 
 class Controller extends Component {
   constructor() {
     super();
+    this.direction = {};
   }
 
   moveRight = player => {
@@ -44,11 +43,28 @@ class Controller extends Component {
     Meteor.call('move.left', player._id);
   };
 
+  componentDidMount() {
+    MoveRightButton.onkeydown = e => {
+        this.direction[e.key] = true;
+    };
+    MoveRightButton.onkeyup = e => {
+        delete this.direction[e.key];
+    };
+    MoveLeftButton.onkeydown = e => {
+        this.direction[e.key] = true;
+    };
+    MoveLeftButton.onkeyup = e => {
+        delete this.direction[e.key];
+    };
+  };
+
   render() {
     console.log(this.props);
     const {currentPlayer} = this.props;
-    return (
-      <div className="root">
+    return (<div className="root">
+    <MetaTags>
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'/>
+    </MetaTags>
         <div className="buttonRow" style={rowStyle}>
           <MoveLeftButton moveLeft={() => this.moveLeft(currentPlayer[0])} />
           <MoveRightButton moveRight={() => this.moveRight(currentPlayer[0])} />
